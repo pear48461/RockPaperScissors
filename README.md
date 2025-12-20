86c088283da415e003ce9e6fba802571c896812b
pear48461 <246436105+pear48461@users.noreply.github.com>
2025-12-19 21:10:21 -0800

    Second pass through tutorial
    5 rounds isn't working and I'm not sure why

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ This is the readme file for my Rock Paper Scissor Project
 
 
 Following walkthrough from https://www.youtube.com/watch?v=A3iZUMDD32w
+
+javascript.js was my original start.
+
+main-original.js is from following the tutorial the first time.
+
+main.js is the current js file - going to follow the tutorial instructions but try to not visually follow his coding line by line
--- main.js
+++ main.js
@@ -1,78 +1,57 @@
 const choices = ["rock", "paper", "scissors"];
-const winners = [];
 
-function game () {
-    for(let i = 1; i <=5; i++){
-        playRound(i);
+function game() {
+    for (let i = 1; i <= 5; i++); {
+    playRound(); 
     }
-    document.querySelector('button').textContent = "Play New Game"
-    logWins();
 }
 
-function playRound(round) {
+function playRound() {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
     const winner = checkWinner(playerSelection, computerSelection);
-    winners.push(winner);
-    logRound(playerSelection, computerSelection, winner, round);
 }
 
 function playerChoice() {
-    let input = prompt("Type Rock, Paper, or Scissors");
-    while(input == null) {
-        input = prompt("Type Rock, Paper, or Scissors");
+    let input = prompt('Type Rock, Paper, or Scissors');
+    while (input == null) {
+       input = prompt('Type Rock, Paper, or Scissors');
     }
     input = input.toLowerCase();
     let check = validateInput(input);
     while (check == false) {
-        input = prompt(
-            "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitalization doesn't matter."
-        );
-     while (input == null) {
-        input = prompt("Type Rock, Paper, or Scissors");
+       input = prompt(
+        "Type Rock, Paper, Scissors. Spelling needs to be exact, but capitalization does not"
+       );
+       while(input == null) {
+       input = prompt('Type Rock, Paper, or Scissors');
     }
-    input = input.toLowerCase();
-    check = validateInput(input);
-}
-return input;
+       input = input.toLowerCase();
+       check = validateInput(input);
+    }
+    return input;
 }
 
 function computerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
+    return choices[Math.floor(Math.random()*choices.length)];
 }
 
-function validateInput(choice) {
-     return choices.includes(choice);
+function validateInput(choice){
+    return choices.includes(choice);
 }
 
-function checkWinner(choiceP, choiceC) {
-    if(choiceP === choiceC) {
-        return 'Tie';
+function checkWinner(choiceP, choiceC){
+    if(choiceP === choiceC){
+        return "Tie";
     } else if (
-        (choiceP === "rock" && choiceC =="scissors") || 
-        (choiceP === "paper" && choiceC =="rock") || 
-        (choiceP === "scissors" && choiceC =="paper")
+        (choiceP === "rock" && choiceC === "scissors") || 
+        (choiceP === "scissors" && choiceC === "paper") || 
+        (choiceP === "paper" && choiceC === "rock")
     ) {
-        return 'Player';
+        return "Player";
     } else {
-        return 'Computer';
+        return "Computer";
     }
 }
 
-function logWins() {
-    let playerWins = winners.filter((item) => item == "Player").length;
-    let computerWins = winners.filter((item) => item == "Computer").length;
-    let ties = winners.filter((item) => item =="Tie").length;
-    console.log('Results');
-    console.log('Player Wins:', playerWins);
-    console.log('Computer Wins', computerWins);
-    console.log('Ties', ties);
-}
-
-function logRound(playerChoice, computerChoice, winner, round) {
-    console.log('Round', round);
-    console.log('Player Chose:', playerChoice);
-    console.log('Computer Chose:', computerChoice);
-    console.log(winner, 'Won the Round');
-    console.log("-----------------------");
-}
+game();
