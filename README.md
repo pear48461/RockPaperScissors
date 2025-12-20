3c70b17fea08326dd0e3526422c38883f55a1cda
pear48461 <246436105+pear48461@users.noreply.github.com>
2025-12-19 20:16:54 -0800

    Finished tutorial and now fully functional
    tutorial was amazing. Now I understand so much
    more.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This is the readme file for my Rock Paper Scissor Project
 
-progressed until the 5 rounds part.
+
 Following walkthrough from https://www.youtube.com/watch?v=A3iZUMDD32w
--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"];
+const winners = [];
 
 function game () {
-    playRound();
+    for(let i = 1; i <=5; i++){
+        playRound(i);
+    }
+    document.querySelector('button').textContent = "Play New Game"
+    logWins();
 }
 
-function playRound() {
+function playRound(round) {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
-    console.log(computerSelection);
-    const winner = checkWinner(playerSelection,computerSelection);
-    console.log(winner);
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
+    logRound(playerSelection, computerSelection, winner, round);
 }
 
 function playerChoice() {
@@ -25,14 +30,15 @@ function playerChoice() {
         );
      while (input == null) {
         input = prompt("Type Rock, Paper, or Scissors");
-    }input = input.toLowerCase();
+    }
+    input = input.toLowerCase();
     check = validateInput(input);
 }
 return input;
 }
 
 function computerChoice() {
-    return choices[Math.floor(Math.random()*choices.length)]
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function validateInput(choice) {
@@ -40,17 +46,33 @@ function validateInput(choice) {
 }
 
 function checkWinner(choiceP, choiceC) {
-    if(choiceP === choiceC){
+    if(choiceP === choiceC) {
         return 'Tie';
     } else if (
         (choiceP === "rock" && choiceC =="scissors") || 
         (choiceP === "paper" && choiceC =="rock") || 
         (choiceP === "scissors" && choiceC =="paper")
-    ){
+    ) {
         return 'Player';
-    } else{
+    } else {
         return 'Computer';
     }
 }
 
-game()
+function logWins() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item =="Tie").length;
+    console.log('Results');
+    console.log('Player Wins:', playerWins);
+    console.log('Computer Wins', computerWins);
+    console.log('Ties', ties);
+}
+
+function logRound(playerChoice, computerChoice, winner, round) {
+    console.log('Round', round);
+    console.log('Player Chose:', playerChoice);
+    console.log('Computer Chose:', computerChoice);
+    console.log(winner, 'Won the Round');
+    console.log("-----------------------");
+}
